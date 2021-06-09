2ee3d28ad532325aec485a688704c805287dca05
paulamal21 <paulinamal21@gmail.com>
2021-06-09 15:36:00 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Lazy Beauty',
+      price: 1000,
+      image: 'Lazy Beauty',
+    },
+    {
+      id: '2',
+      name: 'Lila',
+      price: 1000,
+      image: 'Lila'
+    },
+    {
+      id: '3',
+      name: 'Fluff',
+      price: 1000,
+      image: 'Fluff'
+    }
+  ],
+};
+export default data;
